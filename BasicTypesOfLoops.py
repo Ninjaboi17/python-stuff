6f0a423b22261d3dc4a1330d010cfed70ab967a3
Nachiappan14 <44224013+Nachiappan14@users.noreply.github.com>
2019-10-22 20:13:04 +0530

    Created Basic Types of loops py file

---
+++ BasicTypesOfLoops.py
@@ -0,0 +1,15 @@
+// for loop with the list iterator
+l = [1,2,3,4,5,6,43,125,76]
+for i in l:
+  print i,
+print "\n"
+
+// While loop with the list
+j=0
+while(j<len(l)):
+  print l[j],
+  j+=1
+print "\n"
+
+// continue - goes to the next iteration
+// break - breaks the control out of the current loop
